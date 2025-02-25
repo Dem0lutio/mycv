716bd3ba641a4295b256e1ff7d8b488b423ecb22
luis <luisfelipe@feyh.com.br>
2025-02-24 23:13:35 -0300

    controller_wide_serialization

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -9,6 +9,9 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { UserDto } from 'src/users/dtos/user.dto';
 
+export function Serialize(dto: any) {
+  return UseInterceptors(new SerializeInterceptor(dto));
+}
 export class SerializeInterceptor implements NestInterceptor {
   constructor(private dto: any) {}
   intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -14,9 +14,10 @@ import {
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
-import { SerializeInterceptor } from '../interceptors/serialize.interceptor';
+import { Serialize } from '../interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
   constructor(private userService: UsersService) {}
 
@@ -25,7 +26,6 @@ export class UsersController {
     this.userService.create(body.email, body.password);
   }
 
-  @UseInterceptors(new SerializeInterceptor(UserDto))
   @Get('/:id')
   async findUser(@Param('id') id: string) {
     console.log('handler is running');
