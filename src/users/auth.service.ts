59a904b3c53bda6802e02f77c936a2c1cb35c9da
luis <luisfelipe@feyh.com.br>
2025-02-26 18:04:42 -0300

    auth_service

---
+++ src/users/auth.service.ts
@@ -0,0 +1,21 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { randomBytes, scrypt as _scrypt } from 'crypto';
+import { promisify } from 'util';
+
+const scrypt = promisify(_scrypt);
+
+@Injectable()
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+
+  async signUp(email: string, password: string) {
+    const users = await this.usersService.find(email);
+    if (users.length) {
+      throw new BadRequestException('Email in use');
+    }
+    const salt = randomBytes(8).toString('hex');
+  }
+
+  signIn() {}
+}
