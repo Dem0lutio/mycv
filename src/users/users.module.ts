3934f569ab0e7012a36a9f7ba6493311dbeee368
luis <luisfelipe@feyh.com.br>
2025-02-28 21:59:25 -0300

    globaly_scoped_interceptor

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,3 +1,4 @@
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
@@ -9,6 +10,13 @@ import { AuthService } from './auth.service';
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService, AuthService, CurrentUserInterceptor],
+  providers: [
+    UsersService,
+    AuthService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CurrentUserInterceptor,
+    },
+  ],
 })
 export class UsersModule {}
