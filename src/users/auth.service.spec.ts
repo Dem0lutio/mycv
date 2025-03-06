dcb1dfe1538439d7fda1501c991d8fdae46df7c4
luis <luisfelipe@feyh.com.br>
2025-03-05 21:01:31 -0300

    testing_unused_email

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -3,6 +3,7 @@ import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
 import { User } from './user.entity';
 import { BadRequestException } from '@nestjs/common';
+import exp from 'constants';
 
 describe('AuthService', () => {
   let service: AuthService;
@@ -41,6 +42,12 @@ describe('AuthService', () => {
     fakeUsersService.find = () =>
       Promise.resolve([{ id: 1, email: 'a', password: '1' } as User]);
     await expect(service.signUp('asdf@asdf.com', 'asdf')).rejects.toThrow(
+      BadRequestException,
+    );
+  });
+
+  it('throws if signin is called with an unused email', async () => {
+    await expect(service.signIn('asdf@asdf.com', '1')).rejects.toThrow(
       BadRequestException,
     );
   });
