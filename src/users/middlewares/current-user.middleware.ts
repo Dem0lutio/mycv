ceaf14791649838c17a8456423c9e218c66fe953
Luis Felipe de Azambuja Feyh <luisfelipe@feyh.com.br>
2025-05-30 09:27:45 -0300

    fixing_typescript_error

--- src/users/middlewares/current-user.middleware.ts
+++ src/users/middlewares/current-user.middleware.ts
@@ -1,8 +1,15 @@
-import { CurrentUser } from 'src/users/decorators/current-user.decorator';
 import { UsersService } from './../users.service';
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
+import { User } from '../user.entity';
 
+declare global {
+  namespace Express {
+    interface Request {
+      currentUser?: User;
+    }
+  }
+}
 @Injectable()
 export class CurrentUserMiddleware implements NestMiddleware {
   constructor(private usersService: UsersService) {}
@@ -11,8 +18,7 @@ export class CurrentUserMiddleware implements NestMiddleware {
     const { userId } = req.session || {};
     if (userId) {
       const user = await this.usersService.findOne(userId);
-      // @ts-ignore
-      req.currentUser = user;
+      req.currentUser = user || undefined;
     }
 
     next();
